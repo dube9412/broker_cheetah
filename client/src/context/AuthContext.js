a411d363b9d9c2a0c6acf7e05e724aaf78520d70
Justin Dube <dube9412@gmail.com>
2025-02-04 03:16:26 -0700

    new beginning

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -6,19 +6,22 @@ export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [user, setUser] = useState(null); // Add this line to define the user state variable
 
-  const login = (adminFlag) => {
+  const login = (user, adminFlag) => {
+    setUser(user); // Set the user object in the state
     setIsLoggedIn(true);
     setIsAdmin(adminFlag);
   };
 
   const logout = () => {
+    setUser(null); // Reset the user object in the state
     setIsLoggedIn(false);
     setIsAdmin(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, isAdmin, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, isAdmin, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
