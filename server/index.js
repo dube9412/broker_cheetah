7dc7abfab3a0b982423ed3aa5b8ebbb662963763
Justin Dube <dube9412@gmail.com>
2025-02-10 00:30:41 -0700

    switched to MongDB Atlas Connection

--- server/index.js
+++ server/index.js
@@ -20,9 +20,10 @@ app.use(express.json());
 
 // ✅ Connect to MongoDB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/brokerCheetahDB", {})
-  .then(() => console.log("✅ MongoDB connected"))
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ MongoDB connected!"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 
   app.use('/api/scraper', scraperRoutes);
