7c0c383276efa1a9ee9d56e5baac6ec87d3a53be
Justin Dube <dube9412@gmail.com>
2025-02-10 03:57:33 -0700

    trying again index auth

--- server/index.js
+++ server/index.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dns = require("dns");  // ✅ Move this to the top!
 
-// ✅ Set DNS to prioritize IPv4 (Google & Cloudflare DNS)
-/*dns.setServers(["8.8.8.8", "1.1.1.1"]);
-dns.setDefaultResultOrder("ipv4first");  // This ensures DNS works properly.*/
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://dube9412:dfTtxTuAi2eSZ3ux@brokercheetahdb.rdbel.mongodb.net/?retryWrites=true&w=majority&appName=BrokerCheetahDB";
 
@@ -33,7 +29,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";  // Ignore SSL issues (critical for `mongodb+srv`).
 
 // ✅ Initialize Express
 const app = express();
@@ -41,14 +36,7 @@ const app = express();
 // ✅ Middleware for CORS and JSON parsing
 app.use(cors());
 app.use(express.json());
-/*
-// ✅ MongoDB connection string (manual version, bypassing DNS issues)
-const mongoURI = "mongodb://dube9412:dfTtxTuAi2eSZ3ux@brokercheetahdb-shard-00-00.mongodb.net:27017,brokercheetahdb-shard-00-01.mongodb.net:27017,brokercheetahdb-shard-00-02.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-// ✅ Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB connected!"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));*/
 
 // ✅ Routes
 const scraperRoutes = require('./routes/scraperRoutes');
