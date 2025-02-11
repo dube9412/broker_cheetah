506ab0fd50b013bf5593784f59732988f9f4c8b6
Justin Dube <dube9412@gmail.com>
2025-02-11 03:49:50 -0700

    trying render again updated login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,7 +10,7 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("/api/auth/login", {
+    fetch('https://broker-cheetah-backend.onrender.com/api/auth/login',{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
