361ae6c75fe98ade8838c150e26e83d53b6dfbfc
Justin Dube <dube9412@gmail.com>
2025-02-15 06:40:45 -0700

    index fix url

--- server/index.js
+++ server/index.js
@@ -32,9 +32,13 @@ app.use(express.json());
 
 // ✅ Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/brokerCheetahDB", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB connected"))
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected using MONGO_URI"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 // ✅ API Routes
 app.use("/api/admin", adminRoutes);
