57ae57109db32dc97d52ca2a120e06a0879fccd2
Justin Dube <dube9412@gmail.com>
2025-02-15 21:18:46 -0700

    usecontext import at top

--- client/src/components/AddLender.js
+++ client/src/components/AddLender.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import StatesCheckboxList from "./StatesCheckboxList";
 import { AuthContext } from "../context/AuthContext";
