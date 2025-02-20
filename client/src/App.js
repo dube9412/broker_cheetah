d2c127e0d1698de53ab0334e028585ece3c65fd1
Justin Dube <dube9412@gmail.com>
2025-02-20 08:54:38 -0700

    Testing a change in Edit DSCR for prop type and fixing links in HML class

--- client/src/App.js
+++ client/src/App.js
@@ -60,14 +60,7 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/manage-loan-programs/:lenderId" element={<ManageLoanPrograms />} />
               <Route path="/hard-money-class" element={<HardMoneyClass />} />
-              <Route path="/hard-money-class" element={<HardMoneyClass />} />
-        {lessons.map((lesson) => (
-          <Route
-            key={lesson.id}
-            path={`/hard-money-class/${lesson.id}`}
-            element={<LessonPage lesson={lesson} />} // ✅ Pass lesson as prop
-          />
-        ))}
+              <Route path="/hard-money-class/:lessonId" element={<LessonPage />} />
               <Route path="/fix-and-flip-calculator" element={<FixAndFlipCalc />} />
 
               {/* Loan type & search */}
