668cfedcfe43b011a43d71cf83afe7a1cb6411ed
Justin Dube <dube9412@gmail.com>
2025-02-23 12:41:42 -0700

    wrong logo name

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import logo from "../assets/Broker Cheetah 2.png";
+import logo from "../assets/logo.png";
 import "./NavBar.css";
 
 function NavBar() {
