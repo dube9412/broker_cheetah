9cc06bf888da6a39c2079d8d9dca1ea2fbc06dde
Justin Dube <dube9412@gmail.com>
2025-02-25 01:11:20 -0700

    too many files 2

--- client/src/pages/admin/AdminLenderUsers.js
+++ client/src/pages/admin/AdminLenderUsers.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-import AdminNav from "../../components/AdminNav";
 
 const AdminLenderUsers = () => {
     const [lenderUsers, setLenderUsers] = useState([]);
