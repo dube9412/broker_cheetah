3b53c6e5743c7afd15b16db853efa734cf2bbb09
Justin Dube <dube9412@gmail.com>
2025-03-03 03:00:31 -0700

    fixed mimetypes (extended the list)

--- client/src/components/DocumentUploader.js
+++ client/src/components/DocumentUploader.js
@@ -36,8 +36,9 @@ const DocumentUploader = ({ lenderId, programId, loanPrograms }) => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const validMimeTypes = [
-    "application/pdf", "image/png", "image/jpeg", 
-    "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    "application/pdf", "image/png", "image/jpeg", "image/jpg",
+    "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "text/plain", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 ];
 
 const handleFileChange = (e) => {
