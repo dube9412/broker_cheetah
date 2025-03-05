821dc9ec136da3796abd919945a2faf9db34d05f
Justin Dube <dube9412@gmail.com>
2025-03-04 21:05:52 -0700

    view button page open fix

--- client/src/pages/ManageLoanPrograms.js
+++ client/src/pages/ManageLoanPrograms.js
@@ -235,7 +235,8 @@ console.log("📂 Documents in State:", uploadedDocs);
         uploadedDocs[program._id].map((doc) => (
           <div key={doc._id}>
             📄 ({doc.tag})
-            <button onClick={() => window.open(`https://broker-cheetah-backend.onrender.com${doc.filePath}`, "_blank")}>  View </button>
+            <button onClick={() => window.open(`https://broker-cheetah-backend.onrender.com/api/documents/file/${doc.filename}`, "_blank")}> View </button>
+            {" | "}
             <button onClick={() => handleDeleteDocument(doc._id, program._id)}>Delete</button>
           </div>
         ))
