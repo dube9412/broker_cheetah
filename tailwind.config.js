01264efa6a7b5f2ffb474de18ea657b72974d2e3
Justin Dube <dube9412@gmail.com>
2025-03-17 20:50:15 -0700

    tailwind fixes for css error in vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js", // If your JavaScript files in public use Tailwind classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
