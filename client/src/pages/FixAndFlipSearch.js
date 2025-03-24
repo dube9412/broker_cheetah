0d18891f41112e287e89f8a2ac4aed03e307954a
Justin Dube <dube9412@gmail.com>
2025-03-24 16:50:04 -0700

    search ui improvments

--- client/src/pages/FixAndFlipSearch.js
+++ client/src/pages/FixAndFlipSearch.js
@@ -71,28 +71,28 @@ function FixAndFlipSearch() {
       <fieldset style={{ border: "1px solid #ccc", padding: "15px", marginBottom: "20px" }}>
         <legend><strong>🔹 Deal Details</strong></legend>
 
-        <label>State:<br />
+        <label>State:
           <select value={state} onChange={(e) => setState(e.target.value)} style={{ width: "100%" }}>
             <option value="">-- Select a state --</option>
             {US_STATES.map((st) => (
               <option key={st} value={st}>{st}</option>
             ))}
           </select>
-        </label><br /><br />
+        </label><br />
 
-        <label>Purchase Price:<br />
+        <label>Purchase Price:
           <input value={purchasePrice} onChange={(e) => setPurchasePrice(e.target.value)} style={{ width: "100%" }} />
-        </label><br /><br />
+        </label><br />
 
-        <label>Rehab Needed ($):<br />
+        <label>Rehab Needed ($):
           <input value={rehabNeeded} onChange={(e) => setRehabNeeded(e.target.value)} style={{ width: "100%" }} />
-        </label><br /><br />
+        </label><br />
 
-        <label>ARV:<br />
+        <label>ARV:
           <input value={arv} onChange={(e) => setArv(e.target.value)} style={{ width: "100%" }} />
-        </label><br /><br />
+        </label><br />
 
-        <label>As-Is Value:<br />
+        <label>As-Is Value:
           <input value={asisValue} onChange={(e) => setAsisValue(e.target.value)} style={{ width: "100%" }} />
         </label>
       </fieldset>
@@ -101,15 +101,15 @@ function FixAndFlipSearch() {
       <fieldset style={{ border: "1px solid #ccc", padding: "15px", marginBottom: "20px" }}>
         <legend><strong>🔹 Borrower Profile</strong></legend>
 
-        <label>FICO Score:<br />
+        <label>FICO Score:
           <input value={fico} onChange={(e) => setFico(e.target.value)} style={{ width: "100%" }} />
-        </label><br /><br />
+        </label><br />
 
-        <label>Experience (Flips in past 36 mo):<br />
+        <label>Experience (Flips in past 36 mo):
           <input value={experience} onChange={(e) => setExperience(e.target.value)} style={{ width: "100%" }} />
-        </label><br /><br />
+        </label><br />
 
-        <label>Liquidity (cash, stocks, etc.):<br />
+        <label>Liquidity (cash, stocks, etc.):
           <input value={liquidity} onChange={(e) => setLiquidity(e.target.value)} style={{ width: "100%" }} />
         </label>
       </fieldset>
@@ -118,19 +118,19 @@ function FixAndFlipSearch() {
       <fieldset style={{ border: "1px solid #ccc", padding: "15px", marginBottom: "20px" }}>
         <legend><strong>🔹 Loan Options</strong></legend>
 
-        <label>Recourse:</label><br />
+        <label>Recourse:</label>
         <label><input type="checkbox" checked={recourse.recourse} onChange={() => setRecourse((prev) => ({ ...prev, recourse: !prev.recourse }))} /> Recourse</label><br />
-        <label><input type="checkbox" checked={recourse.nonRecourse} onChange={() => setRecourse((prev) => ({ ...prev, nonRecourse: !prev.nonRecourse }))} /> Non-Recourse</label><br /><br />
+        <label><input type="checkbox" checked={recourse.nonRecourse} onChange={() => setRecourse((prev) => ({ ...prev, nonRecourse: !prev.nonRecourse }))} /> Non-Recourse</label><br />
 
-        <label>Interest Type:</label><br />
-        <label><input type="radio" name="interest" value="dutch" checked={interestType === "dutch"} onChange={(e) => setInterestType(e.target.value)} /> Dutch</label><br />
-        <label><input type="radio" name="interest" value="non-dutch" checked={interestType === "non-dutch"} onChange={(e) => setInterestType(e.target.value)} /> Non-Dutch</label><br /><br />
+        <label>Interest Type:</label>
+        <label><input type="radio" name="interest" value="dutch" checked={interestType === "dutch"} onChange={(e) => setInterestType(e.target.value)} /> Dutch</label><br /?
+        <label><input type="radio" name="interest" value="non-dutch" checked={interestType === "non-dutch"} onChange={(e) => setInterestType(e.target.value)} /> Non-Dutch</label><br />
 
-        <label>Draw Type:</label><br />
+        <label>Draw Type:</label>
         <label><input type="radio" name="drawType" value="dutch" checked={drawType === "dutch"} onChange={(e) => setDrawType(e.target.value)} /> Dutch</label><br />
-        <label><input type="radio" name="drawType" value="non-dutch" checked={drawType === "non-dutch"} onChange={(e) => setDrawType(e.target.value)} /> Non-Dutch</label><br /><br />
+        <label><input type="radio" name="drawType" value="non-dutch" checked={drawType === "non-dutch"} onChange={(e) => setDrawType(e.target.value)} /> Non-Dutch</label><br />
 
-        <label>Cross Collateral Allowed:<br />
+        <label>Cross Collateral Allowed:
           <select value={crossCollateralAllowed} onChange={(e) => setCrossCollateralAllowed(e.target.value)} style={{ width: "100%" }}>
             <option value="">-- Select --</option>
             <option value="yes">Yes</option>
