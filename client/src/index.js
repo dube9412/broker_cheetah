bcd88f1b0d83f1beb2ffeb0462ffbf7a2a2c4c08
Justin Dube <dube9412@gmail.com>
2025-03-27 01:12:38 -0700

    fudge 2

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { LenderAuthProvider } from "./context/LenderAuthContext";
 import "./styles/HardMoneyClass.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <LenderAuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </LenderAuthProvider>
     </AuthProvider>
   </React.StrictMode>
 );
