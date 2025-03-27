853d3b3179f9571a35ba647d01dfa677f60d5638
Justin Dube <dube9412@gmail.com>
2025-03-26 18:36:27 -0700

    ui updates, again

--- client/src/components/AddFixAndFlip.js
+++ client/src/components/AddFixAndFlip.js
@@ -164,23 +164,31 @@ function AddFixAndFlip() {
         <legend><strong>Tiers</strong></legend>
         {tiers.map((tier, index) => (
           <div key={index} style={{ marginBottom: "15px", border: "1px solid #ddd", padding: "10px" }}>
-            
-              <input placeholder="Tier Name" value={tier.tierName} onChange={(e) => handleTierChange(index, "tierName", e.target.value)} />
-            <br />
-              <input placeholder="Fico" value={tier.minFICO} onChange={(e) => handleTierChange(index, "minFICO", e.target.value)} />
-              <input placeholder="Experience" value={tier.minExperience} onChange={(e) => handleTierChange(index, "minExperience", e.target.value)} />
-            <br />
+            <label>Tier Name:
+              <input placeholder="Ex Basis, Gold, Tier 1" value={tier.tierName} onChange={(e) => handleTierChange(index, "tierName", e.target.value)} />
+              </label>
+              <label>FICO:
+              <input placeholder="min" value={tier.minFICO} onChange={(e) => handleTierChange(index, "minFICO", e.target.value)} />
+              </label>
+              <label>Experience:
+              <input placeholder="Completed Deals" value={tier.minExperience} onChange={(e) => handleTierChange(index, "minExperience", e.target.value)} />
+              </label>
+              <label>Loan Range:
               <input placeholder="min" value={tier.loanRange.min} onChange={(e) => handleLoanRangeChange(index, "min", e.target.value)} />
               <input placeholder="max" value={tier.loanRange.max} onChange={(e) => handleLoanRangeChange(index, "max", e.target.value)} />
-            <br />
+              </label>
+              <label>Max LTC:
               <input placeholder="Max Purchase %"value={tier.maxLTC} onChange={(e) => handleTierChange(index, "maxLTC", e.target.value)} />
-            
+              </label>
+              <label>Total or Blended LTC:
               <input placeholder="Total or Blended LTC" value={tier.totalLTC} onChange={(e) => handleTierChange(index, "totalLTC", e.target.value)} />
-            <br />
+              </label>
+              <label>Max ARV:
               <input placeholder="Max ARV" value={tier.maxARV} onChange={(e) => handleTierChange(index, "maxARV", e.target.value)} />
-            
+              </label>
+              <label>Max Rehab %:
               <input placeholder="Max Rehab %" value={tier.rehabPercent} onChange={(e) => handleTierChange(index, "rehabPercent", e.target.value)} />
-            
+              </label>
           </div>
         ))}
         <button type="button" onClick={handleAddTier}>+ Add Tier</button>
